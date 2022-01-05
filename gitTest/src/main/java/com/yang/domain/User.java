7d92ca40214dafebec5bc08527e0da0dfa7e91af
tom <tom@qq.com>
2022-01-05 11:36:16 +0800

    提交一次

---
+++ gitTest/src/main/java/com/yang/domain/User.java
@@ -0,0 +1,11 @@
+package com.yang.domain;
+
+public class User {
+    public static void main(String[] args) {
+        System.out.println("hello  git project1");
+        System.out.println("hello  git project2");
+        System.out.println("hello  git project3");
+        System.out.println("hello  git project4");
+
+    }
+}
